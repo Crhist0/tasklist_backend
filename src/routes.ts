682aa0968ac915b806f40e867403300d3f0e9bf5
Crhistian <crhistiandeoliveira@gmail.com>
2021-11-25 03:05:18 -0300

    fix delete route

--- src/routes.ts
+++ src/routes.ts
@@ -82,7 +82,7 @@ route.put("/saveEdit/", MidsSaveEdit, (req: any, res: any) => {
     });
 });
 
-route.delete("/deleteTask/", (req: any, res: any) => {
+route.delete("/deleteTask/", MidsDeleteTask, (req: any, res: any) => {
     let name = req.body.name;
     let index = req.body.index;
 
