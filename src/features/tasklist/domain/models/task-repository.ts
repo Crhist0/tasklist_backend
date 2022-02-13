70d7c6f3708eae20b8eb5a05ed1ae42367ba3530
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-13 09:35:44 -0300

    corrige interface do repositório de Taks

--- src/features/tasklist/domain/models/task-repository.ts
+++ src/features/tasklist/domain/models/task-repository.ts
@@ -1,8 +1,9 @@
-import { ITask } from "./task";
+import { UpdateResult, DeleteResult } from 'typeorm';
+import { ITask } from './task';
 
 export interface ITaskRepository {
-    readAllOfId(uid: string): Promise<ITask[]>;
-    create(newTask: ITask): Promise<any>;
-    update(uid: string, task: Partial<ITask>): Promise<any>;
-    delete(uid: string): Promise<any>;
+  readAllOfId(uid: string): Promise<ITask[]>;
+  create(newTask: ITask): Promise<ITask>;
+  update(uid: string, task: Partial<ITask>): Promise<UpdateResult>;
+  delete(uid: string): Promise<DeleteResult>;
 }
