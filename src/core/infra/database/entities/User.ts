715600a5e3c8c0d9e8e0065d7f2f71c691cccd11
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-13 09:33:44 -0300

    altera formatação

--- src/core/infra/database/entities/User.ts
+++ src/core/infra/database/entities/User.ts
@@ -1,22 +1,22 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { ITask } from "../../../../features/tasklist/domain/models/task";
-import { IUser } from "../../../../features/user/domain/models/user";
-import { Task } from "./Task";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ITask } from '../../../../features/tasklist/domain/models/task';
+import { IUser } from '../../../../features/user/domain/models/user';
+import { Task } from './Task';
 
 @Entity({
-    name: "user",
-    schema: "public",
+  name: 'user',
+  schema: 'public',
 })
 export class User implements IUser {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({ length: 50 })
-    name: string;
+  @Column({ length: 50 })
+  name: string;
 
-    @Column({ length: 60 })
-    pass: string;
+  @Column({ length: 60 })
+  pass: string;
 
-    @OneToMany(() => Task, (task) => task.user_id)
-    taskList: ITask[];
+  @OneToMany(() => Task, (task) => task.user_id)
+  taskList: ITask[];
 }
