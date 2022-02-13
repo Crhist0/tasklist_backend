27f1c9eacb630c55f2288be2217d667d649a5aa4
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-13 09:34:29 -0300

    altera ok funcion

--- src/core/presentation/helper/http-handler.ts
+++ src/core/presentation/helper/http-handler.ts
@@ -1,34 +1,34 @@
-import { Response } from "express";
-import { DomainError } from "../../domain/errors/domain-error";
-import { ControllerError } from "../error/controller-error";
+import { Response } from 'express';
+import { DomainError } from '../../domain/errors/domain-error';
+import { ControllerError } from '../error/controller-error';
 
-export const ok = (res: Response, data?: any) => {
-    return res.status(200).send({
-        ok: true,
-        data,
-    });
+export const ok = (res: Response, data?: any, code?: number) => {
+  return res.status(code ? code : 200).send({
+    ok: true,
+    data,
+  });
 };
 
 export const serverError = (res: Response, error?: any) => {
-    if (error instanceof DomainError || error instanceof ControllerError) {
-        return res.status(error.code).send({
-            ok: false,
-            error: error.message,
-            identifier: error.name,
-        });
-    }
-
-    if (error instanceof Error) {
-        return res.status(500).send({
-            ok: false,
-            error: error.message,
-            identifier: error.name,
-        });
-    }
-
-    return res.status(500).send({
-        ok: false,
-        error,
-        identifier: "Erro desconhecido.",
+  if (error instanceof DomainError || error instanceof ControllerError) {
+    return res.status(error.code).send({
+      ok: false,
+      error: error.message,
+      identifier: error.name,
     });
+  }
+
+  if (error instanceof Error) {
+    return res.status(500).send({
+      ok: false,
+      error: error.message,
+      identifier: error.name,
+    });
+  }
+
+  return res.status(500).send({
+    ok: false,
+    error,
+    identifier: 'Erro desconhecido.',
+  });
 };
