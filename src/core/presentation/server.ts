34e2854898c74649d09b5acc3355c112a8ab4e95
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-13 09:34:06 -0300

    separa criação e iniciação de server

--- src/core/presentation/server.ts
+++ src/core/presentation/server.ts
@@ -1,13 +1,17 @@
-import express from "express";
-import cors from "cors";
-import { makeRoutes } from "./routes";
+import express from 'express';
+import cors from 'cors';
+import { makeRoutes } from './routes';
+import { Express } from 'express-serve-static-core';
 
-export const initServer = async () => {
-    const app = express();
-    app.use(express.json());
-    app.use(cors());
+export const createServer: () => Express = () => {
+  const app: Express = express();
+  app.use(express.json());
+  app.use(cors());
+  makeRoutes(app);
+  return app;
+};
 
-    makeRoutes(app);
-
-    await app.listen(process.env.PORT || 8081, () => console.log("Servidor rodando..."));
+export const initServer = async (app?: Express | undefined) => {
+  app = app ?? createServer();
+  await app.listen(process.env.PORT || 8081, () => console.log('Servidor rodando...'));
 };
