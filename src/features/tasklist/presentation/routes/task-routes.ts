315505ecef2e87eb7cdd1171a5db4aaeb4110019
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-13 09:55:32 -0300

    altera prettier

--- src/features/tasklist/presentation/routes/task-routes.ts
+++ src/features/tasklist/presentation/routes/task-routes.ts
@@ -1,43 +1,56 @@
-import { Request, Response, Router } from "express";
-import { DeleteTaskUsecase } from "../../domain/usecases/delete-task/delete-task-usecase";
-import { UpdateTaskController } from "./../controllers/update-task-controller";
-import { UpdateTaskUsecase } from "../../domain/usecases/update-task/update-task-usecase";
-import { CreateTaskUsecase } from "../../domain/usecases/create-task/create-task-usecase";
-import { ReadUserTasksUsecase } from "../../domain/usecases/read-user-tasks/read-user-tasks-usecase";
-import { TaskRepository } from "../../infra/repository/task-repository";
-import { CreateTaskController } from "../controllers/create-task-controller";
-import { ReadUserTasksController } from "../controllers/read-user-tasks-controller";
-import { createTaskMids, editTaskMids } from "../middlewares/task-middlewares";
-import { DeleteTaskController } from "../controllers/delete-task-controller";
-import { CacheRepository } from "../../../../core/infra/repositories/cache-repository";
+import { Request, Response, Router } from 'express';
+import { DeleteTaskUsecase } from '../../domain/usecases/delete-task/delete-task-usecase';
+import { UpdateTaskController } from './../controllers/update-task-controller';
+import { UpdateTaskUsecase } from '../../domain/usecases/update-task/update-task-usecase';
+import { CreateTaskUsecase } from '../../domain/usecases/create-task/create-task-usecase';
+import { ReadUserTasksUsecase } from '../../domain/usecases/read-user-tasks/read-user-tasks-usecase';
+import { TaskRepository } from '../../infra/repository/task-repository';
+import { CreateTaskController } from '../controllers/create-task-controller';
+import { ReadUserTasksController } from '../controllers/read-user-tasks-controller';
+import {
+  createTaskMids,
+  deleteTaskMids,
+  editTaskMids,
+  readUserTasksMids,
+} from '../middlewares/task-middlewares';
+import { DeleteTaskController } from '../controllers/delete-task-controller';
+import { CacheRepository } from '../../../../core/infra/repositories/cache-repository';
 
 export class TaskRouter {
-    static getRoutes() {
-        const routes = Router();
+  static getRoutes() {
+    const routes = Router();
 
-        const taskRepository = new TaskRepository();
-        const cacheRepository = new CacheRepository();
+    const taskRepository = new TaskRepository();
+    const cacheRepository = new CacheRepository();
 
-        const readUserTasksUsecase = new ReadUserTasksUsecase(taskRepository, cacheRepository);
-        const readUserTasksController = new ReadUserTasksController(readUserTasksUsecase);
+    const readUserTasksUsecase = new ReadUserTasksUsecase(taskRepository, cacheRepository);
+    const readUserTasksController = new ReadUserTasksController(readUserTasksUsecase);
 
-        const createTaskUsecase = new CreateTaskUsecase(taskRepository, cacheRepository);
-        const createTaskController = new CreateTaskController(createTaskUsecase);
+    const createTaskUsecase = new CreateTaskUsecase(taskRepository, cacheRepository);
+    const createTaskController = new CreateTaskController(createTaskUsecase);
 
-        const updateTasksUsecase = new UpdateTaskUsecase(taskRepository, cacheRepository);
-        const updateTaskController = new UpdateTaskController(updateTasksUsecase);
+    const updateTasksUsecase = new UpdateTaskUsecase(taskRepository, cacheRepository);
+    const updateTaskController = new UpdateTaskController(updateTasksUsecase);
 
-        const deleteTaskUsecase = new DeleteTaskUsecase(taskRepository, cacheRepository);
-        const deleteTaskController = new DeleteTaskController(deleteTaskUsecase);
+    const deleteTaskUsecase = new DeleteTaskUsecase(taskRepository, cacheRepository);
+    const deleteTaskController = new DeleteTaskController(deleteTaskUsecase);
 
-        routes.get("/readTasksByUserId", (req: Request, res: Response) => readUserTasksController.execute(req, res));
+    routes.get('/readTasksByUserId', readUserTasksMids, (req: Request, res: Response) =>
+      readUserTasksController.execute(req, res)
+    );
 
-        routes.post("/", createTaskMids, (req: Request, res: Response) => createTaskController.execute(req, res));
+    routes.post('/', createTaskMids, (req: Request, res: Response) =>
+      createTaskController.execute(req, res)
+    );
 
-        routes.put("/", editTaskMids, (req: Request, res: Response) => updateTaskController.execute(req, res));
+    routes.put('/', editTaskMids, (req: Request, res: Response) =>
+      updateTaskController.execute(req, res)
+    );
 
-        routes.delete("/", (req: Request, res: Response) => deleteTaskController.execute(req, res));
+    routes.delete('/', deleteTaskMids, (req: Request, res: Response) =>
+      deleteTaskController.execute(req, res)
+    );
 
-        return routes;
-    }
+    return routes;
+  }
 }
