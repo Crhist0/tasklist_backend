7d69889a087a04cdc2dc7bcbf9456b3426775203
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-13 09:29:16 -0300

    altera configurações do orm para abranger o sqlite

--- ormconfig.js
+++ ormconfig.js
@@ -1,17 +1,40 @@
-module.exports = {
-    type: "postgres",
-    url: process.env.DATABASE_URL,
+require('dotenv/config');
+
+let config = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  synchronize: false,
+  entities: [process.env.ENTITIES_DIR],
+  migrations: [process.env.MIGRATIONS_DIR],
+  cli: {
+    entitiesDir: 'src/core/infra/database/entities',
+    migrationsDir: 'src/core/infra/database/migrations',
+  },
+  subscribers: [],
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+};
+
+if (process.env.NODE_ENV === 'test') {
+  config = {
+    type: 'sqlite',
+    database: './dbtest.sqlite',
     synchronize: false,
     entities: [process.env.ENTITIES_DIR],
-    migrations: [process.env.MIGRATIONS_DIR],
+    migrations: ['tests/core/infra/database/migrations/**/*.ts'],
     cli: {
-        entitiesDir: "src/core/infra/database/entities",
-        migrationsDir: "src/core/infra/database/migrations",
+      entitiesDir: 'src/core/infra/database/entities',
+      migrationsDir: 'tests/core/infra/database/migrations',
     },
     subscribers: [],
     extra: {
-        ssl: {
-            rejectUnauthorized: false,
-        },
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-};
+  };
+}
+module.exports = config;
