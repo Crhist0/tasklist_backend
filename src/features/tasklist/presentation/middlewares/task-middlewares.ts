95e28e0fac3d61d089259ad4a9a98e36159379d9
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-13 09:53:51 -0300

    melhora verificações

--- src/features/tasklist/presentation/middlewares/task-middlewares.ts
+++ src/features/tasklist/presentation/middlewares/task-middlewares.ts
@@ -1,47 +1,130 @@
-import { Request, Response, NextFunction } from "express";
-import { ControllerError } from "../../../../core/presentation/error/controller-error";
-import { serverError } from "../../../../core/presentation/helper/http-handler";
+import { TokenGenerator } from './../../../../core/infra/adapters/jwt-adapter';
+import { Request, Response, NextFunction } from 'express';
+import { ControllerError } from '../../../../core/presentation/error/controller-error';
+import { serverError } from '../../../../core/presentation/helper/http-handler';
+import { NotAuthorizedError } from '../../domain/errors/token-error';
 
 // middlewares para as rotas
 
 class TaskMiddlewares {
-    static VerifyFields = (req: Request, res: Response, next: NextFunction) => {
-        let description = req.body.description as string;
-        let detail = req.body.detail as string;
+  static VerifyFields = (req: Request, res: Response, next: NextFunction) => {
+    let description = req.body.description as string;
+    let detail = req.body.detail as string;
 
-        if (!description && !detail) {
-            return serverError(res, new ControllerError("Insira uma descrição e um detalhamento.", 403));
-        } else if (!description) {
-            return serverError(res, new ControllerError("Insira uma descrição.", 403));
-        } else if (!detail) {
-            return serverError(res, new ControllerError("Insira um detalhamento.", 403));
-        }
-        next();
-    };
+    if (!description && !detail) {
+      return serverError(res, new ControllerError('Insira uma descrição e um detalhamento.', 403));
+    } else if (!description) {
+      return serverError(res, new ControllerError('Insira uma descrição.', 403));
+    } else if (!detail) {
+      return serverError(res, new ControllerError('Insira um detalhamento.', 403));
+    }
+    next();
+  };
 
-    static VerifyMaxLenght = (req: Request, res: Response, next: NextFunction) => {
-        let description = req.body.description as string;
-        let detail = req.body.detail as string;
+  static VerifyMaxLenght = (req: Request, res: Response, next: NextFunction) => {
+    let description = req.body.description as string;
+    let detail = req.body.detail as string;
 
-        if (Array.from(description).length > 50) {
-            return serverError(res, new ControllerError(`A descrição não pode exceder 50 caracteres. Sua descrição possui ${Array.from(description).length} caracteres.`, 403));
-        }
-        if (Array.from(detail).length > 500) {
-            return serverError(res, new ControllerError(`O detalhamento não pode exceder 500 caracteres. Seu detalhamento possui ${Array.from(detail).length} caracteres.`, 403));
-        }
+    if (Array.from(description).length > 50) {
+      return serverError(
+        res,
+        new ControllerError(
+          `A descrição não pode exceder 50 caracteres. Sua descrição possui ${
+            Array.from(description).length
+          } caracteres.`,
+          403
+        )
+      );
+    }
+    if (Array.from(detail).length > 500) {
+      return serverError(
+        res,
+        new ControllerError(
+          `O detalhamento não pode exceder 500 caracteres. Seu detalhamento possui ${
+            Array.from(detail).length
+          } caracteres.`,
+          403
+        )
+      );
+    }
 
-        next();
-    };
+    next();
+  };
+
+  static VerifyBodyId = (req: Request, res: Response, next: NextFunction) => {
+    let id: string = req.body.id;
+
+    if (!id) {
+      return serverError(res, new ControllerError('Id da tarefa não informado.', 403));
+    }
+    next();
+  };
+
+  static VerifyQueryId = (req: Request, res: Response, next: NextFunction) => {
+    let id = req.query.id as string;
+
+    if (!id) {
+      return serverError(res, new ControllerError('Id da tarefa não informado.', 403));
+    }
+    next();
+  };
+
+  static VerifyBodyToken = (req: Request, res: Response, next: NextFunction) => {
+    let token = req.body.token as string;
+    if (!token) {
+      return serverError(
+        res,
+        new ControllerError('Usuário sem token, necessário novo login.', 403)
+      );
+    }
+    try {
+      TokenGenerator.verifyToken(token);
+    } catch (error) {
+      return serverError(res, new NotAuthorizedError(error as Error));
+    }
+    next();
+  };
+
+  static VerifyQueryToken = (req: Request, res: Response, next: NextFunction) => {
+    let token = req.query.token as string;
+    if (!token) {
+      return serverError(
+        res,
+        new ControllerError('Usuário sem token, necessário novo login.', 403)
+      );
+    }
+    token = JSON.parse(token);
+    try {
+      TokenGenerator.verifyToken(token);
+    } catch (error) {
+      return serverError(res, new NotAuthorizedError(error as Error));
+    }
+    next();
+  };
 }
 
+export let deleteTaskMids = [
+  //
+  TaskMiddlewares.VerifyQueryId,
+  TaskMiddlewares.VerifyQueryToken,
+];
+
 export let createTaskMids = [
-    //
-    TaskMiddlewares.VerifyFields,
-    TaskMiddlewares.VerifyMaxLenght,
+  //
+  TaskMiddlewares.VerifyFields,
+  TaskMiddlewares.VerifyMaxLenght,
+  TaskMiddlewares.VerifyBodyToken,
 ];
 
 export let editTaskMids = [
-    //
-    TaskMiddlewares.VerifyFields,
-    TaskMiddlewares.VerifyMaxLenght,
+  //
+  TaskMiddlewares.VerifyBodyId,
+  TaskMiddlewares.VerifyFields,
+  TaskMiddlewares.VerifyMaxLenght,
+  TaskMiddlewares.VerifyBodyToken,
+];
+
+export let readUserTasksMids = [
+  //
+  TaskMiddlewares.VerifyQueryToken,
 ];
--- src/features/user/presentation/middlewares/user-middlewares.ts
+++ src/features/user/presentation/middlewares/user-middlewares.ts
@@ -1,63 +1,69 @@
-import { Request, Response, NextFunction } from "express";
-import { ControllerError } from "../../../../core/presentation/error/controller-error";
-import { serverError } from "../../../../core/presentation/helper/http-handler";
+import { Request, Response, NextFunction } from 'express';
+import { ControllerError } from '../../../../core/presentation/error/controller-error';
+import { serverError } from '../../../../core/presentation/helper/http-handler';
 
 // middlewares para as rotas de Usuário
-class UserImputValidations {
-    static VerifyNameAndPass = (req: Request, res: Response, next: NextFunction) => {
-        let name = req.body.name as string;
-        let pass = req.body.pass as string;
+class UserInputValidations {
+  static VerifyNameAndPass = (req: Request, res: Response, next: NextFunction) => {
+    let name = req.body.name as string;
+    let pass = req.body.pass as string;
 
-        if (!name && !pass) {
-            return serverError(res, new ControllerError("Informe um nome e uma senha.", 403));
-        }
-        if (!name) {
-            return serverError(res, new ControllerError("Informe um nome.", 403));
-        }
-        if (!pass) {
-            return serverError(res, new ControllerError("Informe uma senha.", 403));
-        }
+    if (!name && !pass) {
+      return serverError(res, new ControllerError('Informe um nome e uma senha.', 403));
+    }
+    if (!name) {
+      return serverError(res, new ControllerError('Informe um nome.', 403));
+    }
+    if (!pass) {
+      return serverError(res, new ControllerError('Informe uma senha.', 403));
+    }
 
-        next();
-    };
+    next();
+  };
 
-    static VerifyRPass = (req: Request, res: Response, next: NextFunction) => {
-        let pass = req.body.pass as string;
-        let Rpass = req.body.Rpass as string;
+  static VerifyRPass = (req: Request, res: Response, next: NextFunction) => {
+    let pass = req.body.pass as string;
+    let Rpass = req.body.Rpass as string;
 
-        if (!Rpass) {
-            return serverError(res, new ControllerError("Repita sua senha.", 403));
-        }
-        if (Rpass != pass) {
-            return serverError(res, new ControllerError("Repita corretamente sua senha.", 403));
-        }
+    if (!Rpass) {
+      return serverError(res, new ControllerError('Repita sua senha.', 403));
+    }
+    if (Rpass != pass) {
+      return serverError(res, new ControllerError('Repita corretamente sua senha.', 403));
+    }
 
-        next();
-    };
+    next();
+  };
 
-    static VerifyLenght = (req: Request, res: Response, next: NextFunction) => {
-        let name = req.body.name as string;
-        let pass = req.body.pass as string;
+  static VerifyLenght = (req: Request, res: Response, next: NextFunction) => {
+    let name = req.body.name as string;
+    let pass = req.body.pass as string;
 
-        if (Array.from(name).length < 5) {
-            return serverError(res, new ControllerError(`O nome ${name} não possui o mínimo de 5 caracteres.`, 403));
-        }
-        if (Array.from(pass).length < 5) {
-            return serverError(res, new ControllerError(`A senha informada não possui o mínimo de 5 caracteres.`, 403));
-        }
-        next();
-    };
+    if (Array.from(name).length < 5) {
+      return serverError(
+        res,
+        new ControllerError(`O nome ${name} não possui o mínimo de 5 caracteres.`, 403)
+      );
+    }
+    if (Array.from(pass).length < 5) {
+      return serverError(
+        res,
+        new ControllerError(`A senha informada não possui o mínimo de 5 caracteres.`, 403)
+      );
+    }
+    next();
+  };
 }
 
 export let createAccMids = [
-    // Middlewares para criação de conta
-    UserImputValidations.VerifyNameAndPass,
-    UserImputValidations.VerifyRPass,
-    UserImputValidations.VerifyLenght,
+  // Middlewares para criação de conta
+  UserInputValidations.VerifyNameAndPass,
+  UserInputValidations.VerifyRPass,
+  UserInputValidations.VerifyLenght,
 ];
 
 export let loginMids = [
-    // Middlewares para login
-    UserImputValidations.VerifyNameAndPass,
-    UserImputValidations.VerifyLenght,
+  // Middlewares para login
+  UserInputValidations.VerifyNameAndPass,
+  UserInputValidations.VerifyLenght,
 ];
