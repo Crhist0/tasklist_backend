7d60c67b1e5eeb8f4366fd02d06925be0869cbf0
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-13 09:56:40 -0300

    altera para readaptar a interface

--- src/features/user/infra/repository/user-repository.ts
+++ src/features/user/infra/repository/user-repository.ts
@@ -5,25 +5,25 @@ import { IUser } from '../../domain/models/user';
 import { IUserRepository } from '../../domain/models/user-repository';
 
 export class UserRepository implements IUserRepository {
-    private userRepository: Repository<IUser>;
+  private userRepository: Repository<IUser>;
 
-    constructor() {
-        this.userRepository = DatabaseConnection.getConnection().manager.getRepository(User);
-    }
+  constructor() {
+    this.userRepository = DatabaseConnection.getConnection().manager.getRepository(User);
+  }
 
-    async findOneByName(name: string): Promise<IUser[]> {
-        return await this.userRepository.find({ name: name });
-    }
+  async findOneByName(name: string): Promise<IUser[]> {
+    return await this.userRepository.find({ name: name });
+  }
 
-    async login(name: string): Promise<IUser> {
-        let userArr = await this.findOneByName(name);
-        let user = userArr[0];
-        return user;
-    }
+  async login(name: string): Promise<Partial<IUser>> {
+    let userArr = await this.findOneByName(name);
+    let user = userArr[0];
+    return user;
+  }
 
-    async create(newUser: IUser): Promise<IUser> {
-        let newUserEntity = this.userRepository.create(newUser);
-        let response = await this.userRepository.save(newUserEntity);
-        return response;
-    }
+  async create(newUser: IUser): Promise<IUser> {
+    let newUserEntity = this.userRepository.create(newUser);
+    let response = await this.userRepository.save(newUserEntity);
+    return response;
+  }
 }
