fbc490b4384b34a8a14cb4ab64ff9ed1d76efa67
Crhistian <crhistiandeoliveira@gmail.com>
2022-05-14 18:08:25 -0300

    add swaggerUI e rota '/docs'

--- src/core/presentation/server.ts
+++ src/core/presentation/server.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import { makeRoutes } from './routes';
 import { Express } from 'express-serve-static-core';
+import swaggerUI from 'swagger-ui-express'
+import swaggerConfigs from '../../../swagger.json'
 
 export const createServer: () => Express = () => {
   const app: Express = express();
   app.use(express.json());
   app.use(cors());
+  app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerConfigs))
   makeRoutes(app);
   return app;
 };
