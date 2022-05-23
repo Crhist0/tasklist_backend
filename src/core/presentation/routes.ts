ae19d4c13c07c447d3dba60d4a0c73dc4afbb25d
Crhistian <crhistiandeoliveira@gmail.com>
2022-05-23 10:00:01 -0300

    refactor: altera caminho do docs

--- src/core/presentation/routes.ts
+++ src/core/presentation/routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { UserRouter } from "../../features/user/presentation/routes/user-routes";
 import { TaskRouter } from "../../features/tasklist/presentation/routes/task-routes";
+import swaggerUI from 'swagger-ui-express'
+import swaggerConfigs from '../../../swagger.json'
 
 export const makeRoutes = (app: express.Application) => {
     app.use("/user", UserRouter.getRoutes());
     app.use("/task", TaskRouter.getRoutes());
+    app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerConfigs))
+
 };
--- src/core/presentation/server.ts
+++ src/core/presentation/server.ts
@@ -2,13 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import { makeRoutes } from './routes';
 import { Express } from 'express-serve-static-core';
-import swaggerUI from 'swagger-ui-express'
-import swaggerConfigs from '../../../swagger.json'
 
 export const createServer: () => Express = () => {
   const app: Express = express();
   app.use(express.json());
-  app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerConfigs))
   app.use(cors());
   makeRoutes(app);
   return app;
